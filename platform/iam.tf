a3ac93ce7e2f524cc81d7d5bc07eb63841b59c4a
Ryan Tracey <ryan.tracey@gmail.com>
2026-02-28 00:03:28 +0100

    Fix gh actions

--- platform/iam.tf
+++ platform/iam.tf
@@ -190,7 +190,8 @@ resource "aws_iam_policy" "github_actions" {
           "s3:GetAccelerateConfiguration",
           "s3:GetBucketRequestPayment",
           "s3:GetBucketLogging",
-          "s3:GetLifecycleConfiguration"
+          "s3:GetLifecycleConfiguration",
+          "s3:GetReplicationConfiguration"
         ]
         Resource = [
           "*"
