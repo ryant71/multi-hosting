0a461e983703d4e0768223e833efc70101507ebc
Ryan Tracey <ryan.tracey@gmail.com>
2026-02-28 11:45:26 +0100

    Fix iam

--- platform/iam.tf
+++ platform/iam.tf
@@ -195,7 +195,8 @@ resource "aws_iam_policy" "github_actions" {
           "s3:GetLifecycleConfiguration",
           "s3:GetReplicationConfiguration",
           "s3:GetEncryptionConfiguration",
-          "s3:GetBucketObjectLockConfiguration"
+          "s3:GetBucketObjectLockConfiguration",
+          "s3:GetBucketTagging"
         ]
         Resource = [
           "*"
